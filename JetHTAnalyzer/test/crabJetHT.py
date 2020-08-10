d379cfe944b97728eacfa54a652ed6abdde20b33
Jussi Viinikainen <jussi.viinikainen@cern.ch>
2020-08-10 18:34:23 +0200

    Made old configuration files compatible with latest executable

--- JetHTAnalyzer/test/crabJetHT.py
+++ JetHTAnalyzer/test/crabJetHT.py
@@ -1,9 +1,8 @@
 from WMCore.Configuration import Configuration
-from CRABClient.UserUtilities import getUsernameFromSiteDB
 config = Configuration()
 
-inputList='crabFilesJetHT10keventsPerIovNewAlignment.txt'
-jobTag='TkAlJetHTAnalysis_Run2017ABCDEF-v1_10keventPerIov_ultralegacy_v2_2019-06-26_includeEta'
+inputList='jetHtFilesForRun2016B.txt'
+jobTag='TkAlJetHTAnalysis_Run2016B_UltraLegacy_2020-07-23'
 
 config.section_("General")
 config.General.requestName = jobTag
@@ -13,20 +12,22 @@ config.General.transferLogs = False
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'JetHTAnalyzer_cfg.py'
+config.JobType.psetName = 'JetHTAnalyzer_jsonImplementation.py'
+config.JobType.pyCfgParams = ['configurationFile=jethtConfiguration.json']
+config.JobType.inputFiles = ['jethtConfiguration.json']
 config.JobType.numCores = 1
-config.JobType.maxMemoryMB = 1500
-config.JobType.maxJobRuntimeMin = 500
+config.JobType.maxMemoryMB = 1800
+config.JobType.maxJobRuntimeMin = 1200
 
 config.section_("Data")
 config.Data.userInputFiles = open(inputList).readlines()
 config.Data.splitting = 'FileBased'
-config.Data.unitsPerJob = 3
+config.Data.unitsPerJob = 5
 config.Data.totalUnits = len(config.Data.userInputFiles)
 config.Data.outputPrimaryDataset = 'TrackAlignment_jetHTanalysis'
-config.Data.outLFNDirBase = ('/store/user/%s/' % (getUsernameFromSiteDB())) + config.General.requestName
+config.Data.outLFNDirBase = '/store/user/jviinika/' + config.General.requestName
 config.Data.publication = False
 
 config.section_("Site")
-config.Site.whitelist = ['T2_US_*']
+config.Site.whitelist = ['T2_US_*','T2_DE_DESY']
 config.Site.storageSite = 'T3_US_FNALLPC'
