0c1154c892e7f3c8ae4e7eb86245f06b2284721c
Jussi Viinikainen <jussi.viinikainen@cern.ch>
2020-08-10 18:31:17 +0200

    Added configuration and file lists for 2016 dataset

---
+++ JetHTAnalyzer/test/crabJetHT_MC2016.py
@@ -0,0 +1,33 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+inputList='jetHtFilesForMC2016_first50inEachPtHat.txt'
+jobTag='TkAlJetHTAnalysis_MC2016_globalTag_2020-08-10'
+
+config.section_("General")
+config.General.requestName = jobTag
+config.General.workArea = config.General.requestName
+config.General.transferOutputs = True
+config.General.transferLogs = False
+
+config.section_("JobType")
+config.JobType.pluginName = 'Analysis'
+config.JobType.psetName = 'JetHTAnalyzer_jsonImplementation.py'
+config.JobType.pyCfgParams = ['configurationFile=jethtConfigurationMC2016_globalTag.json']
+config.JobType.inputFiles = ['jethtConfigurationMC2016_globalTag.json']
+config.JobType.numCores = 1
+config.JobType.maxMemoryMB = 1800
+config.JobType.maxJobRuntimeMin = 1200
+
+config.section_("Data")
+config.Data.userInputFiles = open(inputList).readlines()
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 5
+config.Data.totalUnits = len(config.Data.userInputFiles)
+config.Data.outputPrimaryDataset = 'TrackAlignment_jetHTanalysis'
+config.Data.outLFNDirBase = '/store/user/jviinika/' + config.General.requestName
+config.Data.publication = False
+
+config.section_("Site")
+config.Site.whitelist = ['T2_CH_*']
+config.Site.storageSite = 'T3_US_FNALLPC'
